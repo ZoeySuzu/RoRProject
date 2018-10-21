b9e8beacc6c70da1a3e3209defb0d9dab1386892
ZoeySuzu <Zlls20@sussex.ac.uk>
2018-10-22 00:54:56 +0100

    added create and display actions for article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_170922) do
+ActiveRecord::Schema.define(version: 2018_10_21_171838) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
