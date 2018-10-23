ad1d8e7168d401d9e20e8622b37a32f3e9788da4
ZoeySuzu <Zlls20@sussex.ac.uk>
2018-10-23 20:30:19 +0100

    Added comments on articles and the ability to delete them

--- db/migrate/20181023182716_create_comments.rb
+++ db/migrate/20181023182716_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.references :post, foreign_key: true
+      t.string :commenter
       t.text :body
+      t.references :article, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_171838) do
+ActiveRecord::Schema.define(version: 2018_10_23_182716) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,11 +20,12 @@ ActiveRecord::Schema.define(version: 2018_10_21_171838) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id"
+    t.string "commenter"
     t.text "body"
+    t.integer "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "posts", force: :cascade do |t|
