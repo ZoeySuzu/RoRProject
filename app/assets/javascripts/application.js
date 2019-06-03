afef01acdecf025091bbf318ca4fee62a5200943
ZoeySuzu <Zlls20@sussex.ac.uk>
2019-06-04 00:34:15 +0100

    Updated graphics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
-
-function goToURL(linkstr) {
-    location.href = linkstr;
-  }
-
-  function nProp(e){
-    event.stopPropagation();  
-  }
